392fa9a966d21aaab8768a590101d7d20aec05e8
snowdence <snowdence2911@gmail.com>
2020-08-05 22:57:59 +0700

    Pending framework

--- src/cores/search_strategy.py
+++ src/cores/search_strategy.py
@@ -35,11 +35,7 @@ class MazeGraph:
 class SearchStrategy:
     def __init__(self):
         pass
-
-
-class MimaxAgent():
-    def getAction(self, state):
-        
+    
 
 if __name__ == "__main__":
     print("Search startegy module")
