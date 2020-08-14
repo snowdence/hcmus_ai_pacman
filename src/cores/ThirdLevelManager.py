c369bd2bc1e57fa25b86953440fe90df15ba0120
Brian Drake <hacbangtam@gmail.com>
2020-08-15 01:06:41 +0700

    Monster move 1 step (3x3 with (2,2) is init_pos)

--- src/cores/ThirdLevelManager.py
+++ src/cores/ThirdLevelManager.py
@@ -10,6 +10,13 @@ from cores.search.bfs import *
 from gpath import *
 
 
+from cores.minimax.GameState import GameState
+from cores.minimax.GameStateData import GameStateData
+from cores.minimax.MiniMaxAgent import MiniMaxAgent, AlphaBetaAgent
+from cores.minimax.layout import *
+from cores.minimax.GhostAgents import GhostAgent, DirectionalGhost
+
+
 class ThirdLevelManager:
     ground_group: List[Ground] = []
     player: Player = None
@@ -19,17 +26,19 @@ class ThirdLevelManager:
     result_action_code = []
     started = False
 
-    def __init__(self):
+    # game state
+    game = None
+
+    def __init__(self, game):
         self.map_encode = []
         # list layer
-        w, h = 40, 24
+        #w, h = 10, 10
         # Matrix = [[0 for x in range(w)] for y in range(h)]
-        self.map_tile: List[List[Layer]] = [
-            [0 for x in range(w)] for y in range(h)]
+        # self.map_tile: List[List[Layer]] = [
+        #    [0 for x in range(w)] for y in range(h)]
         self.player = None
-        self.load_map()
+        self.load_map('mini.txt')
         self.parse_map()
-        self.solve_level1_2()
 
         self.step = 0
         self.titleFont = pygame.font.Font(
@@ -37,22 +46,10 @@ class ThirdLevelManager:
         self.itemFont = pygame.font.Font(
             PATH_ASSETS + "font/BD_Cartoon_Shout.ttf", 48)
 
-    def solve_level1_2(self):
-        player_x, player_y = int(self.player.position.x), int(
-            self.player.position.y)
-        coin_x, coin_y = int(self.coin_group[0].position.x), int(
-            self.coin_group[0].position.y)
-        maze_problem = MazeProblem(
-            self.map_encode, MazeState(player_x, player_y), MazeState(coin_x, coin_y))
-        bfs = BFS()
+        self.game = game
+        self.initial_ghost_indexes = game.state.get_ghost_position()
 
-        result, closed, cost = bfs.search(maze_problem, True)
-        self.result = result
-        self.result_action_code = [
-            rslt_item.actionCode for rslt_item in result]
-        print("Solved")
-
-    def load_map(self, file_map='map_lv2.txt'):
+    def load_map(self, file_map='map.txt'):
         game_folder = path.dirname(__file__)
         self.map_data = []
         with open(path.join(game_folder, PATH_MAP + file_map), 'rt') as f:
@@ -66,9 +63,6 @@ class ThirdLevelManager:
             self.map_encode.append(row_p.copy())
         print("load map {} successfully!!!".format(file_map))
 
-    def start(self):
-        self.started = True
-
     def parse_map(self):
         for row, rv in enumerate(self.map_encode, start=0):
             for col, rc in enumerate(self.map_encode[row], start=0):
@@ -79,7 +73,7 @@ class ThirdLevelManager:
                 if ev == '1':
                     # wall
                     self.wall_group.append(Wall(self, Vector2(col, row)))
-                elif ev == '2':
+                elif ev == '.':
                     # food
                     self.coin_group.append(Coin(self, Vector2(col, row)))
                 elif ev == '3':
@@ -89,48 +83,136 @@ class ThirdLevelManager:
                     self.player = Player(self, Vector2(col, row))
         print("Parsed")
 
+    def run(self):
+        game = self.game
+        game.num_moves = 0
+        agent_index = game.start_index
+        num_agents = len(game.agents)
+        while not game.game_over:
+
+            agent = game.agents[agent_index]
+
+            action = None
+
+            observation = game.state.deepcopy()
+
+            action = agent.get_action(observation)
+            game.move_history.append((agent_index, action))
+            game.state = game.state.generate_successor(agent_index, action)
+            game.rules.process(game.state, self)
+            print("Agent {0} action: {1}".format(agent_index, action))
+            print("Epoch agent {0}, Num moves{1}".format(
+                agent_index, game.num_moves))
+
+            if agent_index == num_agents - 1:
+                game.num_moves += 1
+                print("Score :", game.state.data.score)
+                print("Food :", game.state.get_num_food())
+                print(game.state.get_pacman_position())
+                print(game.state.get_ghost_position())
+
+            if agent_index == num_agents - 1:
+                agent_index = 0
+            else:
+                agent_index += 1
+
+            # display update
+        print("End ")
+
     def move_player(self, dx=0, dy=0):
         if (self.player.wall_collision(self.wall_group, dx, dy) is not True):
             self.player.set_position(
                 self.player.position.x + dx, self.player.position.y + dy)
 
+    def update(self):
+        game = self.game
+        num_agents = len(game.agents)
+        if game.game_over:
+            print("End")
+            return
+        for agent_index in range(num_agents):
+            agent = game.agents[agent_index]
+
+            action = None
+            observation = game.state.deepcopy()
+            if agent_index == 0:
+                observation.optimize_mahattan_danger()
+
+            action = agent.get_action(observation)
+            game.move_history.append((agent_index, action))
+            game.state = game.state.generate_successor(agent_index, action)
+            game.rules.process(game.state, self)
+            print("Agent {0} action: {1}".format(agent_index, action))
+            print("Epoch agent {0}, Num moves{1}".format(
+                agent_index, game.num_moves))
+
+            if agent_index == 0:
+                self.move_pacman(game.state.get_pacman_position())
+            elif agent_index > 0:
+                self.move_monster(game.state.get_ghost_state(
+                    agent_index).getPosition())
+
+            if agent_index == num_agents - 1:
+                game.num_moves += 1
+                print("Score :", game.state.data.score)
+                print("Food :", game.state.get_num_food())
+                print(game.state.get_pacman_position())
+                print(game.state.get_ghost_position())
+                if game.state.collide_ghosts_pos(game.state.get_pacman_position()) == True:
+                    game.game_over = True
+                    print("Chet roi!!!!")
+            if game.game_over:
+                print("End")
+                return
+
+            if agent_index == num_agents - 1:
+                agent_index = 0
+
+            else:
+                agent_index += 1
+
+    def move_pacman(self, new_position):
+        x, y = new_position
+        self.player.set_position(x, y)
+        self.player.eat_coin(self.coin_group)
+
+    def move_monster(self, new_position):
+        x, y = new_position
+        for i in range(len(self.monster_group)):
+            mx, my = self.monster_group[i].get_position()
+            adx = abs(mx - x)
+            ady = abs(my - y)
+            if (adx == 1 and ady == 0) or (adx == 0 and ady == 1):
+                ix, iy = self.initial_ghost_indexes[i]
+                bdx = abs(x - ix)
+                bdy = abs(y - iy)
+                ok = [0, 1]
+                if bdx in ok and bdy in ok: 
+                    self.monster_group[i].set_position(x, y)
+                return
+
+
     def render(self, surface):
+
         for ground in self.ground_group:
             ground.render_tile(surface)
 
         for wall in self.wall_group:
             wall.render_tile(surface)
 
+        for coin in self.coin_group:
+            if coin.visiable == True:
+                coin.render_tile(surface)
+
         for monster in self.monster_group:
             monster.render_tile(surface)
 
-        for coin in self.coin_group:
-            coin.render_tile(surface)
-
-        if (self.started == True and self.step < len(self.result)):
-            player_x, player_y = int(self.player.position.x), int(
-                self.player.position.y)
-            ac = self.result[self.step].actionCode
-            self.step += 1
-            if ac == 0:
-                self.move_player(dx=-1)
-                # player_x -= 1
-            elif ac == 1:
-                self.move_player(dy=-1)
-                # player_y -= 1
-            elif ac == 2:
-                self.move_player(dx=1)
-                # player_x += 1
-            elif ac == 3:
-                self.move_player(dy=1)
-                # player_y += 1
-            if self.step == len(self.result):
-                self.coin_group.pop(0)
-
         self.player.render_tile(surface)
+
         text_point = self.titleFont.render(
-            str(self.step) + " $", True, (100, 0, 0))
+            str(self.game.state.get_score()) + " $", True, (100, 0, 0))
         surface.blit(text_point, (0, 0))
+        # pygame.time.wait(10)
 
 
 if __name__ == "__main__":
--- src/screens/ThirdGameScreen.py
+++ src/screens/ThirdGameScreen.py
@@ -8,8 +8,30 @@ from pygame.math import Vector2
 from cores import TileManager, ThirdLevelManager
 from states import *
 
+from cores.minimax.GameState import GameState
+from cores.minimax.GameStateData import GameStateData
+from cores.minimax.MiniMaxAgent import MiniMaxAgent, AlphaBetaAgent
+from cores.minimax.layout import *
+from cores.minimax.GhostAgents import GhostAgent, DirectionalGhost
+
+
+class Game:
+    state: GameState = None
+
+    def __init__(self, agents, rules, start_index=0):
+        self.rules = rules
+        self.agents = agents
+        self.start_index = start_index
+        self.move_history = []
+        self.game_over = False
+        self.total_agent_times = [0 for agent in agents]
+        self.num_moves = 0
+
 
 class ThirdGameScreen(GameScreen):
+    game_over = False
+    init_state: GameState = None
+
     def __init__(self, state):
         GameScreen.__init__(self, state=state)
         self.titleFont = pygame.font.Font(
@@ -17,16 +39,39 @@ class ThirdGameScreen(GameScreen):
         self.itemFont = pygame.font.Font(
             PATH_ASSETS + "font/BD_Cartoon_Shout.ttf", 48)
 
-        print("Created [second level screen]")
-        self.tile_manager = ThirdLevelManager()
+        print("Created [level 3 game screen]")
+        self.run()
 
-    def process_input(self):
-        for event in pygame.event.get():
-            self.on_event(event)
+    def new_game(self, layout, pacman_agent, ghost_agents):
+        agents = [pacman_agent] + ghost_agents[:layout.get_num_ghosts()]
+        init_state = GameState()
+        init_state.initialize(layout, len(ghost_agents))
+
+        game = Game(agents, self, 0)
+        game.state = init_state
+        self.init_state = init_state.deepcopy()
+        return game
+
+    def process(self, state: GameState, game):
+        if state.is_win():
+            game.game_over = True
+        if state.is_lose():
+            game.game_over = True
+
+    def run(self):
+        num_ghost = 2
+        layout = get_layout("maps/mini.txt")
+        pacman = AlphaBetaAgent(3)
+        ghosts = [GhostAgent(i + 1) for i in range(num_ghost)]
+        game = self.new_game(layout, pacman, ghosts)
+        self.tile_manager = ThirdLevelManager(game)
+
+        print("Running")
 
     def on_key_down(self, event):
         if event.key == pygame.K_p:
-            self.tile_manager.start()
+            print("Play press!!!!")
+            self.tile_manager.run()
         if event.key == pygame.K_LEFT:
             self.tile_manager.move_player(dx=-1)
         if event.key == pygame.K_RIGHT:
@@ -37,7 +82,7 @@ class ThirdGameScreen(GameScreen):
             self.tile_manager.move_player(dy=1)
 
     def update(self):
-        pass
+        self.tile_manager.update()
 
     def render(self, window):
         window.fill((0, 0, 0))
